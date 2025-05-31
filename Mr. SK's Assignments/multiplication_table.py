3260121fc2c0f3bb1a13560d0f2bcbf16f0eee2a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-31 23:59:36 +0100

    Multiplication timetable

--- Mr. SK's Assignments/multiplication_table.py
+++ Mr. SK's Assignments/multiplication_table.py
@@ -1,8 +1,9 @@
 for i in range(1, 10): 
-  print(i,'\t')
+  print('\t')
   for j in range(1, 10):
     multiple = (i * j)
-    print(multiple)
+    print(f'{i:.0f} * {j:3} = {multiple:4}',end = "\t ")
+
   print(end = ' ')
  
     
