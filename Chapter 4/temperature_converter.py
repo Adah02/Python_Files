2cb27c733638c098bc65f9fbab756e90090eb41d
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-01 23:37:02 +0100

    Temperature conversion series from celcius to fahrenheit

--- Chapter 4/temperature_converter.py
+++ Chapter 4/temperature_converter.py
@@ -2,5 +2,7 @@ def temperature(celcius):
 	fahrenheit =  (9 / 5) * celcius + 32;
 	return fahrenheit;
 
-celcius = float(input('Enter temperature in degree celcius: '));
-print(f'{temperature(celcius):.1f}');
+print('Celcius \t fahrenheit')
+#celcius = float(input('Enter temperature in degree celcius: '));
+for celcius in range(1, 101, 1):
+	print(f'{celcius} \t {temperature(celcius):.1f}');
