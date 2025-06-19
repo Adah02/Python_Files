95fea159404b3e3848d6e5b4fb75b3b42896238b
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-19 20:52:59 +0100

    Class Exercise

--- Class Works & Quiz/task/class_tasks.py
+++ Class Works & Quiz/task/class_tasks.py
@@ -2,41 +2,37 @@ import random
 
 def length_of_list(numbers):
     numbers_length = 0
-    for item in numbers:
+    for index in numbers:
         numbers_length += 1
 
     return numbers_length
 
 def sum_of_elements_in_even_positions(numbers):
     sum = 0
-    length = 0
-    for index in numbers:
-        length += 1
-
-    for item in range(0, length, 2):
-        sum += numbers[item]
+    counter = 0
+    for item in numbers:
+        if counter % 2 == 0:
+            sum += item
+        counter += 1
     return sum
 
 
 def sum_of_elements_in_odd_positions(numbers):
-    sum_of_odd_index = 0
-    length = 0
-    for index in numbers:
-        length += 1
-
-    for item in range(1, length, 2):
-        sum_of_odd_index += numbers[item]
-    return sum_of_odd_index
+    sum_of_odd = 0
+    counter = 0
+    for item in numbers:
+        if counter % 2 != 0:
+            sum_of_odd += item
+        counter += 1
+    return sum_of_odd
 
 
 def multiple_of_number_in_third_positions(numbers):
     multiple = 1
-    length = 0
-    for index in numbers:
-        length += 1
-
-    for item in range(0, length, 3):
-        multiple *= numbers[item]
+    index = 0
+    for item in numbers:
+        if index % 3 == 0:
+            multiple *= item
     return multiple
 
 def average_of_list(numbers):
@@ -55,11 +51,4 @@ numbers = []
 for i in range(50):
     random_numbers = random.randrange(1, 50)
     numbers.append(random_numbers)
-
-
-print("Number of items in list ",length_of_list(numbers))
-print('Sum of elements in even positions', sum_of_elements_in_even_positions(numbers))
-print("Multiple of numbers at third index: ", multiple_of_number_in_third_positions(numbers))
-
-print("Average of list ",average_of_list(numbers))
 
