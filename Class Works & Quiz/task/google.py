635ab0c8a1d8c8327707ab5149c3a2612b6832fe
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-02 08:46:38 +0100

    Tasks

--- Class Works & Quiz/task/google.py
+++ Class Works & Quiz/task/google.py
@@ -1,16 +1,17 @@
 def google_word(word):
-    items = {alpha:word.count(alpha) for alpha in word}
+    items = {alpha.lower():word.lower().count(alpha) for alpha in word}
     return items
 
 def word_count(word):
     google = {}
     alpha = ''
     for item in word:
-        alpha += (item + ' ')
-    for letter in alpha.split():
-        google[letter] = alpha.count(letter)
+        alpha += item
+    for letter in alpha:
+        google[letter.lower()] = alpha.lower().count(letter)
 
     return google
 
 
-print(word_count("google"))
+print('First method:',google_word("GOogle"))
+print('Second method:',word_count("Google"))
