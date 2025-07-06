21d3850071914ff6556b2b9cc573aba918698d52
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-06 06:25:13 +0100

    Movie Rating System

--- OOP/movie_rating_system/movie.py
+++ OOP/movie_rating_system/movie.py
@@ -3,23 +3,23 @@ from movie_rating_system import rating
 class Movie:
     def __init__(self, title):
         self.title = title
-        self.rate = rating.Rating()
         self.movies = []
 
     @staticmethod
     def movie_date_and_time():
         from datetime import datetime
-        time = datetime.now()
-        hour = time.hour
+        date_time = datetime.now()
+        hour = date_time.hour
         if hour > 12:
             hour = hour % 12
-        date_and_time = time.strftime(f"%d %m %Y  {hour}:%M %p")
+        date_and_time = date_time.strftime(f"%d %m %Y  {hour}:%M %p")
         return date_and_time
 
     def add_movie(self, title):
         #For adding a movie to the list
+        rate = rating.Rating()
         self.movies.append({"name":title, "date": self.movie_date_and_time()})
-        self.rate.add_rating_list([])
+        rate.add_rating_list([])
 
     def movie_list(self):
         return self.movies
