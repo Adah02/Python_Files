710e549da9bd1357b7fd5b15ef15b813a3749016
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-15 23:10:23 +0100

    Movie Rating System Test

--- OOP/movie_rating_system/test/test_movie_rating_app.py
+++ OOP/movie_rating_system/test/test_movie_rating_app.py
@@ -1,4 +1,3 @@
-from movie_rating_system.rating import Rating
 from movie_rating_system.movie_rating import Movie
 from unittest import TestCase
 
@@ -9,25 +8,22 @@ class TestMovieRatingApp(TestCase):
         self.assertEqual(movi.movie_list(), [])
 
     def test_that_rating_list_isEmpty(self):
-        rate = Rating()
-        self.assertEqual(rate.rating_list(), [])
+        movi = Movie()
+        self.assertEqual(movi.rating_list(), [])
 
     def test_that_movie_is_added(self):
         movi = Movie()
         movi.add_movie("Semicolon")
-        self.assertEqual([{'date': '12 07 2025  3:20 PM', 'name': 'Semicolon'}], movi.movie_list())
+        self.assertEqual([{'date': '15 07 2025  11:08 PM', 'name': 'Semicolon'}], movi.movie_list())
 
     def test_that_rating_list_is_added(self):
-        rate = Rating()
         movi = Movie()
         movi.add_movie("Semicolon")
-        rate.add_rating_list([])
-        self.assertEqual([[]], rate.rating_list())
+        self.assertEqual([[]], movi.rating_list())
 
     def test_that_movie_can_be_removed(self):
         movi = Movie()
         movi.add_movie("Semicolon")
-        rate = Rating()
-        rate.add_rating_list([])
-
-        self.assertEqual([[]], rate.rating_list())
+        self.assertEqual([[]], movi.rating_list())
+        movi.remove_movie("Semicolon")
+        self.assertEqual([], movi.rating_list())
